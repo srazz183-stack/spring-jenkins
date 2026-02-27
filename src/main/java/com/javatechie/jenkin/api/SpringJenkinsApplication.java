cf74c4121e8ff030d13882f8992e69844538ea81
Sonu Gorain <srazz183@gmail.com>
2026-02-28 00:01:42 +0530

    first commit

---
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,29 @@
+package com.javatechie.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsApplication {
+
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
